f4caf784315f9de84dab7a1024b67f5ddd55dfec
danselden <danselden@me.com>
2013-03-03 16:33:44 -0500

    My Push

---
+++ inc/header.php
@@ -0,0 +1,34 @@
+<header class="globalHeader">
+	<div class="container">
+		<div class="navbar">
+		  <div class="navbar-inner">
+		    <div class="container">
+		 
+		      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+		      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </a>
+		 
+		      <!-- Be sure to leave the brand out there if you want it shown -->
+		      <a class="brand" href="#"><img src="img/logo.png" width="250"></a>
+		 
+		      <!-- Everything you want hidden at 940px or less, place within here -->
+		      <div class="nav-collapse collapse">
+		      	<ul class="nav">
+				  <li><a href="#">Projects</a></li>
+				  <li><a href="#">People</a></li>
+				  <li><a href="#">Schedule</a></li>
+				  <li><a href="#">Press</a></li>
+				</ul>
+				<form class="navbar-search pull-right">
+				  <input type="text" class="search-query" placeholder="Search">
+				</form>
+		      </div>
+		 
+		    </div>
+		  </div>
+		</div>
+	</div>
+</header>
